3dd76cf0aef5097634ad3836c3aa24aec0e54075
SchizoBoy <cadonienzo@gmail.com>
2021-10-12 19:23:00 +0200

    fonctions auxiliaires yacc ajout pile

--- src/fct_aux_yacc.c
+++ src/fct_aux_yacc.c
@@ -2,8 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
-extern int *tab_format;
+#include "../inc/fct_aux_yacc.h"
 
 // fonction permettant de déterminer combien et quels formats simples se
 // trouvent dans une chaine de caractère
@@ -19,10 +18,41 @@ void format(char *str){
       || *(ptr+1) == 'c'
       || *(ptr+1) == 's'
       ){
+        if(tab_format[0] == MAX_FORMAT){  // Vérification du max de formats
+          fprintf(stderr, "Erreur, trop de formats.\n");
+          exit(-1);
+        }
         tab_format[0]++;
         tab_format[tab_format[0]] = *(ptr+1)-'a';
       }
     }
     ptr++;
   }
+}
+
+// Initialise la pile de région
+void init_pile_region(){
+  pile_region[0] = 1; // Une région dans la pile pour le moment
+  pile_region[1] = 0; // Region 0
+}
+
+// Empile une region
+void empiler_pile_region(int region){
+  if(pile_region[0] == MAX_REGION){ // Vérification du nombre de régions
+    fprintf(stderr, "Erreur, trop de formats.\n");
+    exit(-1);
+  }
+  pile_region[0]++;
+  tab_format[tab_format[0]] = region;
+}
+
+// Dépile une région
+int depiler_pile_region(){
+  pile_region[0]--;
+  return tab_format[tab_format[0]+1];
+}
+
+// Retourne la tête de la pile des regions
+int tete_pile_region(){
+  return tab_format[tab_format[0]+1];
 }
